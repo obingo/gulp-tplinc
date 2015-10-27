b2c97cf1d34dd761bfa9ef672cf8236ed3111878
obingo <xsbchen@qq.com>
2015-10-27 16:35:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 xsbchen
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var gutil = require('gulp-util');
+var through = require('through2');
+var Tplinc = require('tplinc');
+
+var PluginError = gutil.PluginError;
+
+const PLUGIN_NAME = 'gulp-tplinc';
+
+module.exports = function (options) {
+    options = options || {};
+
+    var tplinc = new Tplinc(options);
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            return;
+        }
+
+        var chunk = String(file.contents);
+        chunk = tplinc.process(file.realpath, chunk);
+        file.contents = new Buffer(chunk);
+
+        cb(null, file);
+    });
+};
